a8219de4ea646ea266f0a29a242c54aeaa67d312
ryabuily <ryabuily@fit.cvut.cz>
2021-01-11 13:21:38 +0100

    Theme Model and Builder docs add

--- src/main/java/fit/biepjv/agilediary/models/Theme.java
+++ src/main/java/fit/biepjv/agilediary/models/Theme.java
@@ -1,10 +1,8 @@
 package fit.biepjv.agilediary.models;
 
-import javax.swing.text.html.parser.Entity;
-
-/*
-    \class Theme class
-    \brief Class describing the Theme
+/**
+    <b>Theme class</b>
+    Class describing the Theme
 
     According to Agile methodology there's a biggest entity called theme.
     That could represent the company department or something bigger.
@@ -12,27 +10,63 @@ import javax.swing.text.html.parser.Entity;
  */
 public class Theme extends EntityAbstract{
 
+    /**
+     * <b>Theme Builder class</b>
+     * This is a class that is used to build a Theme class.
+     */
     public static class ThemeBuilder extends EntityBuilderAbstract {
-        String fieldName;
-        String fieldDescription;
+        /**
+         * Name of the Theme
+         */
+        String themeName;
 
+        /**
+         *  Description of the Theme
+         */
+        String themeDescription;
+
+        /**
+         * Name setter
+         * @param name Name the needs to be set in the Builder
+         * @return The same object with changed property
+         */
         public ThemeBuilder name(String name){
-            fieldName = name;
+            themeName = name;
             return this;
         }
+
+        /**
+         * Description setter
+         * @param description Description of the Theme that needs to be set to the Builder
+         * @return The same object with changed property
+         */
         public ThemeBuilder description(String description){
-            fieldDescription = description;
+            themeDescription = description;
             return this;
         }
+
+        /**
+         * Build method
+         * @return Object that was build
+         */
+        @Override
         public EntityAbstract build(){
             return new Theme(this);
         }
     }
 
+    /**
+     * Default Theme constructor
+     */
     public Theme(){
         super();
     }
+
+    /**
+     * Theme constructor
+     * @param builder Builder that is used to build object
+     */
     public Theme(ThemeBuilder builder){
-        super(builder.fieldName, builder.fieldDescription);
+        super(builder.themeName, builder.themeDescription);
     }
 }
