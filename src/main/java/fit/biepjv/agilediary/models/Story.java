e1b6d78b04fd3444b0a763851d743c84d228e365
ryabuily <ryabuily@fit.cvut.cz>
2021-01-11 15:40:30 +0100

    Story Model&Builder docs add

--- src/main/java/fit/biepjv/agilediary/models/Story.java
+++ src/main/java/fit/biepjv/agilediary/models/Story.java
@@ -4,22 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-/* \class
-    \brief
+/** <b>Story class</b>
 
     This is the smallest implementation of issue. It stands for a single task (or story) which can be used
-    further to build something bigger
+    further to build something bigger. It can contain substories, however, there are not implemented yet.
  */
 public class Story extends IssueAbstract{
-    List<Story> substories; /// list of included smaller stories (might be empty)
+    /**
+     *  List of included smaller stories (might be empty)
+     */
+    List<Story> substories;
 
+    /** <b>Story Builder class</b>
+     * This is the builder of Story object.
+     */
     public static class StoryBuilder extends IssueBuilderAbstract{
+        /**
+         * List of included stories aka substories
+         */
         List<Story> fieldSubstories = new ArrayList<>();
 
+        /**
+         * Method that sets list of stories that are included
+         * @param stories List of stories
+         * @return Current Builder object
+         */
         public StoryBuilder substories(List<Story> stories){
             fieldSubstories = stories;
             return this;
         }
+
+        /**
+         * Method that adds story to the list of included stories
+         * @param story Story to be added
+         * @return Current Builder object
+         */
         public StoryBuilder addSubstory(Story story){
             fieldSubstories.add(story);
             return this;
@@ -30,37 +49,58 @@ public class Story extends IssueAbstract{
         }
     }
 
+    /**
+     * Default constructor
+     */
     public Story(){
         super();
         substories = new ArrayList<>();
     }
+
+    /**
+     * Constructor based on name
+     * @param name Story name
+     */
     public Story(String name){
         super(name);
         substories = new ArrayList<>();
     }
+
+    /**
+     * Constructor based on name and description
+     * @param name Story name
+     * @param description Story description
+     */
     public Story(String name, String description){
         super(name, description);
         substories = new ArrayList<>();
     }
+
+    /**
+     * Constructor based on builder
+     * @param builder Story builder object
+     */
     public Story(StoryBuilder builder){
         super(builder);
         substories = builder.fieldSubstories;
     }
 
     //getters
+    @Override
     public List<Story> getSubIssues() {
         return substories;
     }
 
     //setters
+    @Override
     public void setSubIssues(List<? extends IssueAbstract> substories){
-        //setUpdatedAt();
         for (IssueAbstract substory : substories) {
             addIssue(substory);
         }
     }
+
+    @Override
     public void addIssue(IssueAbstract issue){
-        //setUpdatedAt();
         if(issue instanceof Story)
             substories.add((Story) issue);
     }
